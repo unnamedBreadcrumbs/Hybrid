316792a0bdcff5b2cc0afe2318fda601d9de16a6
Ronan Stam <Ronan.stam@gmail.com>
2023-01-18 20:41:28 +0100

    voicelines

---
+++ CrashnDash hybrid2/Assets/Dialogue/FirstOption.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstOption : MonoBehaviour
+{
+    public bool selected;
+    public AudioClip dialogue;
+    public AudioSource source;
+    public GameObject currentscreen;
+    public GameObject nextscreen;
+    public bool audioplaying = false;
+
+    public GameObject sprite;
+    public SpriteRenderer sr;
+    public Sprite select;
+    public Sprite deselect;
+
+    public NPC_AnimationKeyBinds MinerAnim;
+
+    void Start()
+    {
+        NPC_AnimationKeyBinds MinerAnim = gameObject.GetComponent<NPC_AnimationKeyBinds>();
+    }
+
+    void Update()
+    {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+
+        if (horizontalInput < 0)
+        {
+            selected = true;
+
+        }
+        if (horizontalInput > 0)
+        {
+            selected = false;
+        }
+
+
+        if (selected == true)
+        {
+            sr.sprite = select;
+        }
+        if (selected == false)
+        {
+            sr.sprite = deselect;
+        }
+
+
+
+
+        if (selected == true && Input.GetButton("Fire1") && audioplaying == false)
+        {
+            //audio.clip = dialogue;
+            //audio.Play();
+            StartCoroutine(Play());
+        }
+
+        if (audioplaying == true)
+        {
+            MinerAnim.MouthCount = 1;
+        }
+        else
+        {
+            MinerAnim.MouthCount = 0;
+        }
+    }
+
+    IEnumerator Play()
+    {
+
+        sprite.SetActive(false);
+        audioplaying = true;
+        source.clip = dialogue;
+        source.Play();
+        MinerAnim.Move();
+        yield return new WaitForSeconds(source.clip.length);
+        MinerAnim.Stop();
+        nextscreen.SetActive(true);
+        currentscreen.SetActive(false);
+
+
+    }
+
+}
